4e8a0da3884459fbd60f88db5395e946c652baa6
ajajajaj <arjun.boss@gmail.com>
2018-03-21 19:02:28 -0700

    initial changes

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var users = require('./routes/users');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser())
+
+app.use('/api/createdeal', users);
+app.listen(3001, () => console.log('Example app listening on port 3001!'))
+module.exports = app;
+
